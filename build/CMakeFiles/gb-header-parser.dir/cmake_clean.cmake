643b58c0115053a3c3ed4a997074492f5f48eac2
Joe Kenyon <joekenyon72@gmail.com>
2026-06-13 22:16:59 +0100

    s

---
+++ build/CMakeFiles/gb-header-parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gb-header-parser.dir/main.cpp.o"
+  "CMakeFiles/gb-header-parser.dir/main.cpp.o.d"
+  "gb-header-parser"
+  "gb-header-parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gb-header-parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
