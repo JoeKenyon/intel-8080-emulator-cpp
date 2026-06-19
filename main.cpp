d8c513f0487de176a3707d84a58049d792e15230
Joe Kenyon <joekenyon72@gmail.com>
2026-06-19 20:40:54 +0100

    saasdasdsa

--- main.cpp
+++ main.cpp
@@ -11,5 +11,23 @@ int main()
         return 1;
     }
 
+    cpu.PC = 0x0100;
+
+    for(int i = 0; i < 10; i++)
+    {
+        uint8_t opcode = cpu.memory[cpu.PC];
+        uint8_t operand_1 = cpu.memory[cpu.PC + 1];
+        uint8_t operand_2 = cpu.memory[cpu.PC + 2];
+
+        cpu.PC += cpu.OP_TABLE[opcode].size;
+
+        cpu.execute(opcode, operand_1, operand_2);
+
+        std::cout << cpu.OP_TABLE[opcode].mnemonic << std::endl;
+    }
+
+
+
+
     return 0;
 }
