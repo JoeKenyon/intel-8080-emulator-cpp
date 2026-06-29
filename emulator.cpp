9fb8e63d4d7b4c5b3596accc82de1f66ef8bd435
Joe Kenyon <joekenyon72@gmail.com>
2026-06-29 20:31:12 +0100

    some more opcodes

--- emulator.cpp
+++ emulator.cpp
@@ -63,6 +63,47 @@ bool step()
         case OP_JM : op_JM (op1, op2); break;
         case OP_JP : op_JP (op1, op2); break;
 
+        case OP_ADI: op_ADI(op1); break;
+
+        // add
+        case OP_ADD_A: op_ADD(regs.A); break;
+        case OP_ADD_B: op_ADD(regs.B); break;
+        case OP_ADD_C: op_ADD(regs.C); break;
+        case OP_ADD_D: op_ADD(regs.D); break;
+        case OP_ADD_E: op_ADD(regs.E); break;
+        case OP_ADD_H: op_ADD(regs.H); break;
+        case OP_ADD_L: op_ADD(regs.L); break;
+        case OP_ADD_M: op_ADD(memory[(regs.H << 8) | regs.L]); break;
+
+        case OP_SUB_A: op_SUB(regs.A); break;
+        case OP_SUB_B: op_SUB(regs.B); break;
+        case OP_SUB_C: op_SUB(regs.C); break;
+        case OP_SUB_D: op_SUB(regs.D); break;
+        case OP_SUB_E: op_SUB(regs.E); break;
+        case OP_SUB_H: op_SUB(regs.H); break;
+        case OP_SUB_L: op_SUB(regs.L); break;
+        case OP_SUB_M: op_SUB(memory[(regs.H << 8) | regs.L]); break;
+
+        case OP_SUI: op_SUI(op1); break;
+
+        case OP_INR_A: op_INR(regs.A); break;
+        case OP_INR_B: op_INR(regs.B); break;
+        case OP_INR_C: op_INR(regs.C); break;
+        case OP_INR_D: op_INR(regs.D); break;
+        case OP_INR_E: op_INR(regs.E); break;
+        case OP_INR_H: op_INR(regs.H); break;
+        case OP_INR_L: op_INR(regs.L); break;
+        case OP_INR_M: op_INR(memory[(regs.H << 8) | regs.L]); break;
+
+        case OP_DCR_A: op_DCR(regs.A); break;
+        case OP_DCR_B: op_DCR(regs.B); break;
+        case OP_DCR_C: op_DCR(regs.C); break;
+        case OP_DCR_D: op_DCR(regs.D); break;
+        case OP_DCR_E: op_DCR(regs.E); break;
+        case OP_DCR_H: op_DCR(regs.H); break;
+        case OP_DCR_L: op_DCR(regs.L); break;
+        case OP_DCR_M: op_DCR(memory[(regs.H << 8) | regs.L]); break;
+
         default: std::cerr << "Unimplemented opcode: 0x"<< std::hex << (int)opcode << " at PC=0x" << PC << "\n"; return false;
     }
 
--- opcodes_arithmetic.cpp
+++ opcodes_arithmetic.cpp
@@ -1,2 +1,86 @@
 #include "opcodes.h"
 #include "hardware.h"
+
+
+void op_ADI(uint8_t op1)
+{
+    // add immediate
+    // calculate aux carry befor they change
+    flags.AuxCarry = __calculateAuxCarryAdd(regs.A, op1);
+    uint16_t result = (uint16_t)regs.A + op1;
+    flags.Carry = (result > 0xFF);
+    regs.A = (uint8_t)result;
+
+    flags.Zero   = __calculateZero(regs.A);
+    flags.Sign   = __calculateSign(regs.A);
+    flags.Parity = __calculateParity(regs.A);
+
+    PC += 2;
+}
+void op_ADD(uint8_t src)
+{
+    // add immediate
+    // calculate aux carry befor they change
+    flags.AuxCarry = __calculateAuxCarryAdd(regs.A, src);
+    uint16_t result = (uint16_t)regs.A + src;
+    flags.Carry = (result > 0xFF);
+    regs.A = (uint8_t)result;
+
+    flags.Zero   = __calculateZero(regs.A);
+    flags.Sign   = __calculateSign(regs.A);
+    flags.Parity = __calculateParity(regs.A);
+
+    PC += 1;
+}
+void op_SUI(uint8_t op1)
+{
+    flags.AuxCarry = __calculateAuxCarrySub(regs.A, op1);
+    flags.Carry = (regs.A < op1);
+    regs.A = regs.A - op1;
+
+    flags.Zero   = __calculateZero(regs.A);
+    flags.Sign   = __calculateSign(regs.A);
+    flags.Parity = __calculateParity(regs.A);
+
+    PC += 2;
+}
+void op_SUB(uint8_t src)
+{
+    flags.AuxCarry = __calculateAuxCarrySub(regs.A, src);
+    flags.Carry = (regs.A < src);
+    regs.A = regs.A - src;
+
+    flags.Zero   = __calculateZero(regs.A);
+    flags.Sign   = __calculateSign(regs.A);
+    flags.Parity = __calculateParity(regs.A);
+
+    PC += 1;
+}
+void op_INR(uint8_t& reg)
+{
+    // calculate Auxiliary Carry out of bit 3 before the value increments
+    flags.AuxCarry = __calculateAuxCarryAdd(reg, 1);
+    reg += 1;
+
+    // Update status flags, carry flag left alone
+    flags.Zero   = __calculateZero(reg);
+    flags.Sign   = __calculateSign(reg);
+    flags.Parity = __calculateParity(reg);
+
+    // INR is a 1-byte instruction
+    PC += 1;
+}
+
+void op_DCR(uint8_t& reg)
+{
+    // calculate Auxiliary Carry out of bit 3 before the value chanegs
+    flags.AuxCarry = __calculateAuxCarrySub(reg, 1);
+    reg -= 1;
+
+    // Update status flags, carry flag left alone
+    flags.Zero   = __calculateZero(reg);
+    flags.Sign   = __calculateSign(reg);
+    flags.Parity = __calculateParity(reg);
+
+    PC += 1;
+}
