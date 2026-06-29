b669dd1882d33088c88f0d2f20ed9500389afd08
Joe Kenyon <joekenyon72@gmail.com>
2026-06-29 19:38:02 +0100

    ea

--- emulator.h
+++ emulator.h
@@ -1,6 +1,5 @@
 #include "hardware.h"
 #include <string>
 
-// Simple procedural functions (defined in your implementation file)
 bool loadRom(const std::string& filename, uint16_t startAddress);
 bool step();
--- main.cpp
+++ main.cpp
@@ -3,16 +3,14 @@
 
 int main()
 {
-    // Load the binary directly at CP/M's standard base address
+    // load test rom
     if (!loadRom("roms/cpudiag.bin", 0x0100)) {
         return 1;
     }
 
-    // Set initial machine state pointers
     PC = 0x0100;
     SP = 0xF000;
 
-    // Run the first step!
     while(step());
 
     std::cout << "\nStep complete. Next PC target is: 0x" << std::hex << PC << "\n";
